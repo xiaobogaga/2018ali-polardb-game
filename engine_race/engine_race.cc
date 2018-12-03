f142ed9271ea19e28fb57e0109afab2e8e94651a
xiaobogaga <m18202787110@gmail.com>
2018-12-03 10:44:20 +0800

    bugs

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -204,7 +204,7 @@ namespace polar_race {
         char* ans = NULL;
         for (int i = 0; i < parties; i++) {
             j = -1;
-            printInfo(stderr, "[EngineRace] : range read part %d\n", i);
+            printInfo(stderr, "[EngineRace-%d] : range read part %d\n", i, part);
             ans = this->queue->get(i, &j, &partSize, &keyPointer);
             for (j++; j <= partSize; j++) {
                 if (ans == NULL) break;
